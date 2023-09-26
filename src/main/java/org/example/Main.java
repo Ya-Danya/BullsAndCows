cb355b54c8ab08ed33b06aa3c854ff7bdb688407
Ya-Danya <ddsamsonenko@edu.hse.ru>
2023-09-26 23:22:38 +0300

    Algorithm almost done.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -58,10 +58,13 @@ public class Main {
 
     public static void bullsNCows (int[] secret) {
         List<int[]> tries = new ArrayList<int[]>();
-        List<int[]> compares = new ArrayList<int[]>();
+        ArrayList<int[]> compares = new ArrayList<int[]>();
         int[] bulls = {-1, -1, -1, -1};
         int bulls_amount = 0;
-        int[] cows = {-1, -1, -1, -1};
+        ArrayList<Integer>[] cows = new ArrayList[4];
+
+        int cows_iterator = 0;
+
         int cows_amount = 0;
 
         int[] ans = {-1, -1, -1, -1};
@@ -71,29 +74,66 @@ public class Main {
         compares.add(new int[4]);
         compares.set(0, compareArrs(secret, tries.get(0)));
         for (int i = 0; i < 4; i++) {
-            if (tries.get(0)[i] == 2) {
-                ans[i] = compares.get(0)[i];
+            if (compares.get(0)[i] == 2) {
+                ans[i] = tries.get(0)[i];
+                bulls_amount++;
+            } else if(compares.get(0)[i] == 1) {
+                cows[cows_amount].set(0, tries.get(0)[i]);
+                cows[cows_amount].add(i);
+                cows_amount++;
             }
         }
+        System.out.println("First combination is 0123");
         // Вторая попытка угадывания.
         tries.add(new int[4]);
         tries.set(1, new int[]{4, 5, 6, 7});
+        compares.add(new int[4]);
+        compares.set(1, compareArrs(secret, tries.get(1)));
         for (int i = 0; i < 4; i++) {
-            if (tries.get(0)[i] == 2) {
-                ans[i] = compares.get(0)[i];
+            if (compares.get(1)[i] == 2) {
+                ans[i] = tries.get(1)[i];
+                bulls_amount++;
+            } else if(compares.get(1)[i] == 1) {
+                cows[cows_amount].set(0, tries.get(1)[i]);
+                cows[cows_amount].add(i);
+                cows_amount++;
             }
         }
-        compareArrs(secret, tries.get(1));
-        compares.add(new int[4]);
-        compares.set(0, compareArrs(secret, tries.get(1)));
+
+
+        System.out.println("Second combination is 4567");
         // Третяя попытка угадывания.
-        if (cows_amount == 4) {
+        tries.add(new int[4]);
 
-        } else if (cows_amount == 3) {
-
-        } else {
-
+        // Формируем комбинацию.
+        int place_of_first_cow = cows[0].get(0);
+        for (int i = 0; i < 4; i++) {
+            if (i != place_of_first_cow) {
+                tries.get(2)[i] = cows[0].get(0);
+                cows_iterator++;
+            } else {
+                if (cows_amount == 4) {
+                    tries.get(2)[i] = cows[1].get(0); // дописать этот случай
+                }
+            }
         }
+
+        compares.add(new int[4]);
+        compares.set(2, compareArrs(secret, tries.get(2)));
+
+        // Проверка на быков и коров.
+        for (int i = 0; i < 4; i++) {
+            if (tries.get(2)[i] == 2) {
+                ans[i] = compares.get(1)[i];
+                bulls_amount++;
+            } else if(tries.get(2)[i] == 1) {
+                cows[cows_amount].set(0, compares.get(2)[i]);
+                cows_amount++;
+            }
+        }
+
+        // Четвертая попытка.
+        tries.add(new int[4]);
 
 
 
